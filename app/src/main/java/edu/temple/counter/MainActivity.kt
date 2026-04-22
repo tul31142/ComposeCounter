50eb98601425a2d80066fef81a1f0b999ff17169
Alexis Graham <tul31142@temple.edu>
2026-04-22 15:15:02 -0400

    Finished lab activity

--- app/src/main/java/edu/temple/counter/MainActivity.kt
+++ app/src/main/java/edu/temple/counter/MainActivity.kt
@@ -4,15 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -24,11 +32,12 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CounterTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold(modifier = Modifier.fillMaxSize()) {innerPadding ->
                     Counter(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
+
             }
         }
     }
@@ -36,19 +45,46 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Counter(modifier: Modifier = Modifier) {
-    Column (modifier = modifier.fillMaxSize(), verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally) {
+
+    var counterVal : Int by rememberSaveable { mutableStateOf(0) }
+
+    Column(horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center, modifier = modifier.fillMaxSize()) {
+        Button(
+            modifier = modifier.padding(16.dp),
+            onClick = {
+                counterVal++
+
+            }
+        ) {
+            Image(painterResource(android.R.drawable.arrow_up_float), "Up arrow")
+        }
         Text(
-            text = "0",
-            fontSize = 192.sp
+            text = counterVal.toString(),
+            fontSize = 72.sp,
+            modifier = modifier
         )
+        Button(
+            modifier = modifier.padding(16.dp),
+            onClick = {
+                if(counterVal > 0){
+                    counterVal--
+                }
+
+            }
+        ) {
+            Image(painterResource(android.R.drawable.arrow_down_float), "Down arrow")
+        }
+
     }
+
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    CounterTheme {
+fun CounterPreview() {
+    CounterTheme (dynamicColor = false){
+        val context = LocalContext.current
         Counter()
     }
 }
